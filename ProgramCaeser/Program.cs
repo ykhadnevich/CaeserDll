ffbede1be4a4cc99b0da0f91e591c7c8b47103de
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2022-10-24 11:07:01 +0200

    Added Dll and did Program.cs again

---
+++ ProgramCaeser/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Class;
+var a = new Class1();
+
+Console.WriteLine("Enter value (1) or (2)");
+int val = Convert.ToInt32(Console.ReadLine());
+switch (val)
+{
+    case 1:
+        Console.WriteLine("Please enter message to encrypt: ");
+        string myString = Console.ReadLine();
+        myString = myString.ToLower();
+        char[] message = myString.ToCharArray();
+        Console.WriteLine("Please enter int: ");
+        int key = Convert.ToInt32(Console.ReadLine());
+        string secret = a.Encrypt(message, key);
+        Console.WriteLine($"Secrete-code:{secret}");
+        break;
+    case 2 :
+        Console.WriteLine("Please enter message to decrypt: "); 
+        string msgString = Console.ReadLine();
+        msgString = msgString.ToLower();
+        char[] messages = msgString.ToCharArray();
+        Console.WriteLine("Please enter int: ");
+        int keys = Convert.ToInt32(Console.ReadLine());
+        string secrets = a.Decrypt(messages, keys);
+        Console.WriteLine($"Secrete-code:{secrets}");
+        break;
+}
